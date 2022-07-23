233d049b607aa7aea5f488e563fc05807152ab13
hartie95 <mail@hartie95.de>
2022-07-23 19:23:32 +0200

    Refactored the element switching a bit: * It now uses the propper skill repo for the male/female traveler * Added support for Dendro (and other future elements)

--- src/main/java/test/gc/switchele/commands/SwitchElement.java
+++ src/main/java/test/gc/switchele/commands/SwitchElement.java
@@ -4,20 +4,47 @@ import emu.grasscutter.GameConstants;
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
 import emu.grasscutter.data.GameData;
+import emu.grasscutter.data.excels.AvatarSkillDepotData;
 import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.server.packet.send.PacketSceneEntityAppearNotify;
 import test.gc.switchele.Switchele;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Command(label = "switchelement", description = "Switch element for traveller", usage = "switchelement [White/Anemo/Geo/Electro]", aliases = {"se"}, threading = true)
+@Command(label = "switchelement", description = "Switch element for traveller", usage = "switchelement [White/Anemo/Geo/Electro/Dendro]", aliases = {"se"}, threading = true)
 public class SwitchElement implements CommandHandler {
+
+    private Element getElementFromString(String elementString) {
+        return switch (elementString.toLowerCase()) {
+            case "white", "common" -> Element.COMMON;
+            case "fire", "pyro" -> Element.FIRE;
+            case "water", "hydro" -> Element.WATER;
+            case "wind", "anemo" -> Element.WIND;
+            case "ice", "cryo" -> Element.ICE;
+            case "rock", "geo" -> Element.ROCK;
+            case "electro" -> Element.ELECTRO;
+            case "grass", "dendro", "plant" -> Element.GRASS;
+            default -> null;
+        };
+    }
+
+    private boolean changeAvatarElement(Player sender, int avatarId, Element element) {
+        Avatar avatar = sender.getAvatars().getAvatarById(avatarId);
+        AvatarSkillDepotData skillDepot = GameData.getAvatarSkillDepotDataMap().get(element.getSkillRepoId(avatarId));
+        if (avatar == null || skillDepot == null) {
+            return false;
+        }
+        avatar.setSkillDepotData(skillDepot);
+        avatar.setCurrentEnergy(1000);
+        avatar.save();
+        return true;
+    }
+
     @Override
     public void execute(Player sender, Player targetPlayer, List<String> args) {
         if (args.size() != 1) {
-            CommandHandler.sendMessage(sender, "Usage: /se OR /switchelement [White/Anemo/Geo/Electro]");
+            CommandHandler.sendMessage(sender, "Usage: /se OR /switchelement [White/Anemo/Geo/Electro/Dendro]");
             return;
         }
         if (sender == null) {
@@ -25,46 +52,25 @@ public class SwitchElement implements CommandHandler {
             return;
         }
 
-        String element = args.get(0);
+        Element element = getElementFromString(args.get(0));
 
-        Integer elementId = switch (element.toLowerCase()) {
-            case "white" -> 501;
-            case "anemo" -> 504;
-            case "geo" -> 506;
-            case "electro" -> 507;
-            default -> null;
-        };
 
-        if (elementId == null) {
+        if (element == null) {
             CommandHandler.sendMessage(sender, "Invalid element");
             return;
         }
 
-        List<Integer> id = new ArrayList<>();
-        id.add(GameConstants.MAIN_CHARACTER_MALE);
-        id.add(GameConstants.MAIN_CHARACTER_FEMALE);
-
-
-        try {
-            for (int i : id) {
-                Avatar avatar
-                        = sender.getAvatars().getAvatarById(i);
-                if (avatar != null) {
-                    avatar.setSkillDepotData(GameData.getAvatarSkillDepotDataMap().get(elementId));
-                    avatar.setCurrentEnergy(1000);
-                    avatar.save();
-                }
-            }
-            String success = String.format("Successfully changed element to %s", element);
-            CommandHandler.sendMessage(sender,success);
+        boolean maleSuccess = changeAvatarElement(sender, GameConstants.MAIN_CHARACTER_MALE, element);
+        boolean femaleSuccess = changeAvatarElement(sender, GameConstants.MAIN_CHARACTER_FEMALE, element);
+        if (maleSuccess || femaleSuccess) {
+            String success = String.format("Successfully changed element to %s", element.name());
+            CommandHandler.sendMessage(sender, success);
             int scene = sender.getSceneId();
             sender.getWorld().transferPlayerToScene(sender, 1, sender.getPos());
             sender.getWorld().transferPlayerToScene(sender, scene, sender.getPos());
             sender.getScene().broadcastPacket(new PacketSceneEntityAppearNotify(sender));
-        } catch (Exception ignored) {
-            ignored.printStackTrace();
+        } else {
+            CommandHandler.sendTranslatedMessage(sender, "Failed to change the Element.");
         }
-
-
     }
 }
