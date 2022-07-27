1a7054bc2e029cbce5ae670d8d51ecd9198a2f66
Penelopeep <julek.zolich@gmail.com>
2022-07-27 19:21:04 +0200

    No more checking for console usage

--- src/main/java/test/gc/switchele/LanguageHelper.java
+++ src/main/java/test/gc/switchele/LanguageHelper.java
@@ -8,19 +8,12 @@ import java.nio.charset.StandardCharsets;
 
 public class LanguageHelper{
     public static String reader(String text, String username) {
-        String fileName;
-        if(username == "gc-console"){
-            fileName = String.format("%s.json", Grasscutter.getLanguage().getLanguageCode().replace("-","_"));
-        }
-        else {
-            fileName = String.format("%s.json", Grasscutter.getGameServer().getAccountByName(username).getLocale());
-        }
+        String fileName = String.format("%s.json", Grasscutter.getGameServer().getAccountByName(username).getLocale());
         ClassLoader classLoader = Switchele.getInstance().getClass().getClassLoader();
 
         try (InputStream inputStream = classLoader.getResourceAsStream(fileName);
              InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(streamReader)) {
-             Switchele.getInstance().getLogger().info(fileName);
              text = new JsonParser().parse(reader).getAsJsonObject().get(text).getAsString();
 
         } catch (IOException e) {
