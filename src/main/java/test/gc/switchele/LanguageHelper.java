8dd15794387fbe1cb464d8ac281ab5bdd41ae2e3
Penelopeep <julek.zolich@gmail.com>
2022-09-16 11:27:06 +0200

    Fixed Null warning in IDE

--- src/main/java/test/gc/switchele/LanguageHelper.java
+++ src/main/java/test/gc/switchele/LanguageHelper.java
@@ -3,8 +3,12 @@ package test.gc.switchele;
 import com.google.gson.JsonParser;
 import emu.grasscutter.Grasscutter;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class LanguageHelper{
     public static String reader(String text, String username) {
@@ -12,7 +16,7 @@ public class LanguageHelper{
         ClassLoader classLoader = Switchele.getInstance().getClass().getClassLoader();
 
         try (InputStream inputStream = classLoader.getResourceAsStream(fileName);
-             InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             InputStreamReader streamReader = new InputStreamReader(Objects.requireNonNull(inputStream), StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(streamReader)) {
              text = new JsonParser().parse(reader).getAsJsonObject().get(text).getAsString();
 
